a0fbc7a303b3cfeb426d8499109bec5400bcc68f
RobinQu <robinqu@gmail.com>
2024-07-05 09:57:19 +0800

    chores: use gcc11 in apt source

--- .devcontainer/install-dev-tools.sh
+++ .devcontainer/install-dev-tools.sh
@@ -2,7 +2,16 @@ set -e
 set -x
 
 apt update
-apt install -y wget vim
+apt install -y wget vim ssh bash-completion git build-essential
+
+echo """if ! shopt -oq posix; then
+  if [ -f /usr/share/bash-completion/bash_completion ]; then
+    . /usr/share/bash-completion/bash_completion
+  elif [ -f /etc/bash_completion ]; then
+    . /etc/bash_completion
+  fi
+fi
+""" >> /root/.bashrc
 
 mkdir -p ~/miniconda3  && \
     wget https://repo.anaconda.com/miniconda/Miniconda3-latest-$(uname -s)-$(uname -m).sh -O ~/miniconda3/miniconda.sh  && \
@@ -13,5 +22,5 @@ echo "export PATH=/root/miniconda3/bin:\$PATH" >> /root/.bashrc
 
 source /root/.bashrc
 
-/root/miniconda3/bin/conda create -y --name instinct-cpp -c conda-forge python=3.11 conan cmake gcc=12 gxx=12 git
-echo "conda activate instinct-cpp" >> /root/.bashrc
+/root/miniconda3/bin/conda create -y --name system-gcc -c conda-forge python=3.11 conan cmake
+echo "conda activate system-gcc" >> /root/.bashrc
